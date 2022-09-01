ecc854c31443175d6d3e8bc59f0c35efe493b802
bastelbude-01 <messerschmidt.markus@web.de>
2022-09-01 18:01:07 +0200

    work

--- vision_rpi_bot/vision_rpi_bot/cmd_to_pwm_driver.py
+++ vision_rpi_bot/vision_rpi_bot/cmd_to_pwm_driver.py
@@ -1,18 +1,47 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Int16
+from std_msgs.msg import Twist
 
 class VelocitySubscriber(Node):
 
     def __init__(self):
         super().__init__('cmd_vel_subscriber')
-        self.subscription = self.create_subscription(Int16,'pub_topic',self.cmd_to_pwm_callback,
+        self.subscription = self.create_subscription(Twist,'pub_topic',self.cmd_to_pwm_callback,
             10)
-        self.subscription  
+        self.subscription 
+
+        self.right_motor_en = 5
+        self.right_motor_a = 13
+        self.right_motor_b = 6
+
+        self.left_motor_a = 19
+        self.left_motor_b = 16
+        self.left_motor_en = 26
+        self.second = 2
+        
+
+        GPIO.setup(right_motor_en , GPIO.OUT)
+        GPIO.setup(right_motor_a  , GPIO.OUT)
+        GPIO.setup(right_motor_b , GPIO.OUT)    
+
+        GPIO.setup(left_motor_a , GPIO.OUT)
+        GPIO.setup(left_motor_b , GPIO.OUT)
+        GPIO.setup(left_motor_en , GPIO.OUT)
+
+
+        self.pwm_r = GPIO.PWM(right_motor_en, 1000)
+        self.pwm_l = GPIO.PWM(left_motor_en, 1000)
+
+        self.self.pwm_r.start(75)
+        self.self.pwm_l.start(75) 
 
     def cmd_to_pwm_callback(self, msg):
-        self.get_logger().info('I heard: "%d"' % msg.data)
+
+        right_wheel_vel = (msg.linear.x + msg.angular.y) /2
+        left_wheel_vel = (msg.linear.x - msg.angular.y) /2
+        
+        print("right_wheel_vel ," / "left_wheel_vel")
 
 
 def main(args=None):
